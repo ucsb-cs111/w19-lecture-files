daa63ef6e09edfa9b828c3b62629b51e9e0223fa
John R. Gilbert <gilbert@cs.ucsb.edu>
2019-01-16 13:42:58 -0800

    fix bug from lecture, choose pivot using absolute value

--- 00.AllPythonCode/cs111.py
+++ 00.AllPythonCode/cs111.py
@@ -248,30 +248,49 @@ def Usolve(U, y):
     return x
 
 ########################################################################
-def LUsolve(A, b):
-    """Solve a linear system Ax = b for x by LU factorization with partial pivoting.
+def LUfactor(A, pivoting = True):
+    """Factor a square matrix with partial pivoting, A[p,:] == L @ U
     Parameters: 
       A: the matrix.
-      b: the right-hand side
+      pivoting: whether or not to do partial pivoting, default True
     Outputs (in order):
-      x: the computed solution
-      rel_res: relative residual norm,
-        norm(b - Ax) / norm(b)
+      L: the lower triangular factor, same dimensions as A, with ones on the diagonal
+      U: the upper triangular factor, same dimensions as A
+      p: the permutation vector that permutes the rows of A by partial pivoting
     """
+    
     # Check the input
     m, n = A.shape
     assert m == n, 'input matrix A must be square'
     
-    # LU factorization
-    L, U, p = LUfactor(A)
+    # Initialize p to be the identity permutation
+    p = np.array(range(n))
     
-    # Forward and back substitution
-    y = Lsolve(L,b[p])
-    x = Usolve(U,y)
+    # Make a copy of the matrix that we will transform into L and U
+    LU = A.astype(np.float64).copy()
     
-    # Residual norm
-    rel_res = npla.norm(b - A@x) / npla.norm(b)
+    # Eliminate each column in turn
+    for piv_col in range(n):
+        
+        # Choose the pivot row and swap it into place
+        if pivoting:
+            piv_row = piv_col + np.argmax(np.abs(LU[piv_col:, piv_col]))
+            assert LU[piv_row, piv_col] != 0., "can't find nonzero pivot, matrix is singular"
+            LU[[piv_col, piv_row], :]  = LU[[piv_row, piv_col], :]
+            p[[piv_col, piv_row]]      = p[[piv_row, piv_col]]
+            
+        # Update the rest of the matrix
+        pivot = LU[piv_col, piv_col]
+        assert pivot != 0., "pivot is zero, can't continue"
+        for row in range(piv_col + 1, n):
+            multiplier = LU[row, piv_col] / pivot
+            LU[row, piv_col] = multiplier
+            LU[row, (piv_col+1):] -= multiplier * LU[piv_col, (piv_col+1):]
+            
+    # Separate L and U in the result
+    U = np.triu(LU)
+    L = LU - U + np.eye(n)
     
-    return (x, rel_res)
+    return (L, U, p)
 
 ########################################################################
